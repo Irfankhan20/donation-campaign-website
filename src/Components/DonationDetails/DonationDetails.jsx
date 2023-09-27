aaacf195eb673db9f0db2b15a487e4b39902ec30
Irfankhan20 <sipathan96@gmail.com>
2023-09-27 19:17:17 +0600

    toast add in donation button

--- src/Components/DonationDetails/DonationDetails.jsx
+++ src/Components/DonationDetails/DonationDetails.jsx
@@ -1,27 +1,41 @@
 
+
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const DonationDetails = () => {
   const donations = useLoaderData();
   const { id } = useParams();
   const idInt = parseInt(id);
   const oneDonation = donations.find((oneDonation) => oneDonation.id === idInt);
-  console.log(oneDonation);
 
   const buttonStyle = {
     backgroundColor: oneDonation.text_color,
+  };
+
+  const handleButtonClick = () => {
+    // Show a toast notification when the button is clicked
+    toast.success("Thank you for your donation!", {
+      position: toast.POSITION.TOP_CENTER,
+      autoClose: 2000, // Automatically close the toast after 2 seconds
+    });
   };
 
   return (
     <div className="mt-20 relative">
       {/* Image */}
       <div className="w-full">
-        <img className="w-full" src={oneDonation.image} alt="" />
+        <img className="w-full h-[750px]" src={oneDonation.image} alt="" />
       </div>
 
       {/* Overlay Container */}
-      <div className="absolute -bottom-[876px] left-20 w-full h-full opacity-100 transition-opacity">
-        <button className="py-2 px-4 rounded text-white" style={buttonStyle}>
+      <div className="absolute -bottom-[630px] left-20 w-full h-full opacity-100 transition-opacity">
+        <button
+          className="py-2 px-4 rounded text-white"
+          style={buttonStyle}
+          onClick={handleButtonClick}
+        >
           Donate ${oneDonation.price}
         </button>
       </div>
@@ -31,19 +45,14 @@ const DonationDetails = () => {
         <h2 className="text-4xl font-bold pt-14 pb-6">{oneDonation.title}</h2>
         <p className="pb-32 font-normal">{oneDonation.description}</p>
       </div>
+
+      {/* Add the ToastContainer component */}
+      <ToastContainer />
     </div>
   );
 };
 
 export default DonationDetails;
-
-
-
-
-
-
-
-
 
 
 
---
+++ src/Components/utility/localstorage.js
@@ -0,0 +1,19 @@
+const getStoredDonation = () =>{
+    const storedDonation = localStorage.getItem('donation-category');
+    if(storedDonation){
+        return JSON.parse(storedDonation);
+    }
+    return [];
+}
+
+
+const saveJobApplication = id =>{
+    const storedDonations = getStoredDonation();
+    const exists = storedDonations.find(donationId => donationId === id);
+    if(!exists){
+        storedDonations.push(id);
+        localStorage.setItem('donation-category', JSON.stringify(storedDonations))
+    }
+}
+
+export { getStoredDonation, saveJobApplication}
